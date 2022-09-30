b655ea0e15fc06c86bf42a0421a73f3cf916f710
Sasal66 <107730821+Sasal66@users.noreply.github.com>
2022-09-30 12:54:28 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -13,22 +13,22 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //=================================================//
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/c/HwModsWa857'
-global.ig = 'https://www.instagram.com/hikal_857/?hl=id'
-global.mygc = 'wa.me/6285714170944'
-global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
-global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
-global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
-global.email = 'denyp857@gmail.com'
+global.youtube = '😈'
+global.ig = 'https://www.instagram.com/defan_sugiarto'
+global.mygc = 'wa.me/6281321776496'
+global.myweb = '😈'
+global.linkgrupss = "https://chat.whatsapp.com/JVFN0SYnXLg23qjvickkdM"
+global.linkpro = '😈'
+global.email = 'defansugiarto5@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'ᴹᴿ᭄defα͢͢͢ή彡' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'ANIABOZ' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'ᴹᴿ᭄defα͢͢͢ή彡' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6285729877941']
-global.premium = ['6285729877941']
+global.owner = ['6281321776496']
+global.premium = ['6281321776496 ']
 global.packname = 'ANIABOZ'
 global.ownerr = ['ᴹᴿ᭄defα͢͢͢ή彡']
 global.author = 'ANIABOT'
